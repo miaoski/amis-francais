4bd791ec14b9f9861d3b0da92dd74498d3b1f288
Lin Zhemin <lin.zhemin@gmail.com>
2015-08-24 09:04:06 +0800

    add table log

--- hocr/toufu.sql
+++ hocr/toufu.sql
@@ -2,4 +2,5 @@ PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
 CREATE TABLE toufu ( p int, par text, bbox text, words text, cnt int );
 CREATE INDEX p_par on toufu (p, par);
+CREATE TABLE log ( p int, par text, ans text );
 COMMIT;
