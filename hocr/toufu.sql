d93cabb9f650261aa15fedfde231e1e1fe867034
Lin Zhemin <lin.zhemin@gmail.com>
2015-08-24 09:08:41 +0800

    rename words to ans

--- hocr/toufu.sql
+++ hocr/toufu.sql
@@ -1,6 +1,6 @@
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
-CREATE TABLE toufu ( p int, par text, bbox text, words text, cnt int );
+CREATE TABLE toufu ( p int, par text, bbox text, ans text, cnt int );
 CREATE INDEX p_par on toufu (p, par);
 CREATE TABLE log ( p int, par text, ans text );
 COMMIT;
